7b88d8dee49e23cdf738bf3bdb27d897bc424556
Lakshya Kumar <lakshyak983@gmail.com>
2021-01-10 22:36:46 +0530

    backend

---
+++ todoapi/apis/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+
+from todos import models
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
